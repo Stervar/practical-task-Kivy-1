8073743ed1c18f0f53983ba4dfa4916e8fe66216
❃𝘝𝘢𝘥𝘪𝘮_𝘎𝘢𝘣𝘦𝘳𝘬𝘰𝘳𝘯❃ <150718158+Stervar@users.noreply.github.com>
2025-04-07 01:03:06 +0300

    Image

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from kivy.app import App
-from kivy.uix.label import Label
+# from kivy.app import App
+# from kivy.uix.label import Label
  
-class MainApp(App):
-    def build(self):
-        label = Label(text='Hello from Kivy',
-                      size_hint=(.5, .5),
-                      pos_hint={'center_x': .12, 'center_y': .10})
+# class MainApp(App):
+#     def build(self):
+#         label = Label(text='Hello from Kivy',
+#                       size_hint=(.5, .5),
+#                       pos_hint={'center_x': .12, 'center_y': .10})
  
-        return label
+#         return label
  
-if __name__ == '__main__':
-    app = MainApp()
-    app.run()
+# if __name__ == '__main__':
+#     app = MainApp()
+#     app.run()
