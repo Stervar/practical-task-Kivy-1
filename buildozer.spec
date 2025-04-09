6b0c1ca40693e508cb85eca39715c74d2d7fd9db
❃𝘝𝘢𝘥𝘪𝘮_𝘎𝘢𝘣𝘦𝘳𝘬𝘰𝘳𝘯❃ <150718158+Stervar@users.noreply.github.com>
2025-04-09 21:57:30 +0300

    vg

--- buildozer.spec
+++ buildozer.spec
@@ -1,6 +1,16 @@
 
 [app]
-title = My Application
-package.name = myapp
+title = MyKivyApp
+package.name = mykivyapp
+package.domain = org.test
+source.dir = .
 source.include_exts = py,png,jpg,kv,atlas
+version = 0.1
 requirements = python3,kivy
+orientation = portrait
+presplash.filename = %(source.dir)s/data/presplash.png
+icon.filename = %(source.dir)s/data/icon.png
+
+[buildozer]
+log_level = 2
+warn_on_root = 1
--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-# from kivy.app import App
-# from kivy.uix.label import Label
- 
-# class MainApp(App):
-#     def build(self):
-#         label = Label(text='Hello from Kivy',
-#                       size_hint=(.5, .5),
-#                       pos_hint={'center_x': .12, 'center_y': .10})
- 
-#         return label
- 
-# if __name__ == '__main__':
-#     app = MainApp()
-#     app.run()
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+    def build(self):
+        return Label(text="Hello, World!")
+
+if __name__ == "__main__":
+    MyApp().run()
